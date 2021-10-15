c5f978ab4e0d31adbbd15cfb92791db1d1e22e20
Akmal Alikhujaev <akmal@viaware.nl>
2021-10-15 21:20:04 +0200

    Add javadoc for Address.java

--- src/main/java/com/akmal/springfoodieappbackend/model/Address.java
+++ src/main/java/com/akmal/springfoodieappbackend/model/Address.java
@@ -8,6 +8,16 @@ import javax.persistence.Embeddable;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
+/**
+ * Class encapsulates the address object. The logic was outsourced
+ * to be further reused in multiple components of the system.
+ * The class is embeddable and therefore, the Spring Data JPA will
+ * flatten the object and include it as columns into the embedded object.
+ * Fields that are not validated are optional.
+ * @author Akmal ALikhujaev
+ * @version 1.0
+ * @since 1.0
+ */
 @Getter
 @NoArgsConstructor(force = true)
 @AllArgsConstructor
