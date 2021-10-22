0bca9afd9bdcc5d0ed43ab24ae3dbe1477eb6598
Akmal Alikhujaev <akmal@viaware.nl>
2021-10-22 21:04:59 +0200

    [REFACTOR] Change the name of OpeningTimesRepository class to OpeningTimeRepository

--- src/main/java/com/akmal/springfoodieappbackend/repository/OpeningTimeRepository.java
+++ src/main/java/com/akmal/springfoodieappbackend/repository/OpeningTimeRepository.java
@@ -1,15 +1,15 @@
 package com.akmal.springfoodieappbackend.repository;
 
-import com.akmal.springfoodieappbackend.model.OpeningTimes;
+import com.akmal.springfoodieappbackend.model.OpeningTime;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
- * The class is a Spring Data Jpa repository for the OpeningTimes entity.
+ * The class is a Spring Data Jpa repository for the OpeningTime entity.
  * @author Akmal Alikhujaev
  * @version 1.0
  * @created 22/10/2021 - 8:24 PM
  * @project Spring Foodie App Backend
  * @since 1.0
  */
-public interface OpeningTimesRepository extends JpaRepository<OpeningTimes, Long> {
+public interface OpeningTimeRepository extends JpaRepository<OpeningTime, Long> {
 }
