cd0c7e16495fa371277a1884a343d999e6f53e65
Akmal Alikhujaev <akmal@viaware.nl>
2021-10-22 20:39:16 +0200

    [ADD] Create CartItemRepository

---
+++ src/main/java/com/akmal/springfoodieappbackend/repository/CartItemRepository.java
@@ -0,0 +1,15 @@
+package com.akmal.springfoodieappbackend.repository;
+
+import com.akmal.springfoodieappbackend.model.CartItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * The class is a Spring Data Jpa repository that represents CartItem Entity.
+ * @author Akmal Alikhujaev
+ * @version 1.0
+ * @created 22/10/2021 - 8:33 PM
+ * @project Spring Foodie App Backend
+ * @since 1.0
+ */
+public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+}
