0cb94ee93e55dfb7f94df85a16bc9ff9d988a4f3
Akmal Alikhujaev <akmal@viaware.nl>
2021-10-22 20:40:41 +0200

    [ADD] Create OrderRepository

---
+++ src/main/java/com/akmal/springfoodieappbackend/repository/OrderRepository.java
@@ -0,0 +1,15 @@
+package com.akmal.springfoodieappbackend.repository;
+
+import com.akmal.springfoodieappbackend.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * The class is a Spring Data Repository that represents the Order Entity.
+ * @author Akmal Alikhujaev
+ * @version 1.0
+ * @created 22/10/2021 - 8:32 PM
+ * @project Spring Foodie App Backend
+ * @since 1.0
+ */
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
