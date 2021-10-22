c42de53f3f1de67fefa28b7540799c883f9c0b1a
Akmal Alikhujaev <akmal@viaware.nl>
2021-10-22 21:01:36 +0200

    [ADD] Create AddressDto

---
+++ src/main/java/com/akmal/springfoodieappbackend/dto/AddressDto.java
@@ -0,0 +1,18 @@
+package com.akmal.springfoodieappbackend.dto;
+
+/**
+ * DTO class implemented using Java Record Classes representing Address entity
+ * @author Akmal Alikhujaev
+ * @version 1.0
+ * @created 22/10/2021 - 8:46 PM
+ * @project Spring Foodie App Backend
+ * @since 1.0
+ */
+public record AddressDto(String country,
+                         String city,
+                         String postCode,
+                         String street,
+                         String addition,
+                         String apartmentNumber,
+                         LocationDto locationDto) {
+}
