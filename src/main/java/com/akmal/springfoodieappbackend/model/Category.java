7747dbd1b191dcdffd348e4d6ea7599637997dc0
Akmal Alikhujaev <akmal@viaware.nl>
2021-10-22 21:02:53 +0200

    [REFACTOR] Removed unnecessary relationship between Category and the Restaurant
    Restaurants can be queried based on the category already, therefore, there is no need in keeping restaurant as a dependency in Category class.

--- src/main/java/com/akmal/springfoodieappbackend/model/Category.java
+++ src/main/java/com/akmal/springfoodieappbackend/model/Category.java
@@ -4,11 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * Class represents restaurant's category.
@@ -35,6 +36,4 @@ public class Category {
   @NotBlank(message = "Description is required")
   @Size(min = 5)
   private String description;
-  @ManyToMany(fetch = FetchType.LAZY)
-  private final Set<Restaurant> restaurants = new HashSet<>();
 }
