80a070327e4b3b5e853b68b31d51fd89512f1483
Akmal Alikhujaev <akmal@viaware.nl>
2021-10-22 20:40:27 +0200

    [ADD] Create OptionSetRepository

---
+++ src/main/java/com/akmal/springfoodieappbackend/repository/OptionSetRepository.java
@@ -0,0 +1,15 @@
+package com.akmal.springfoodieappbackend.repository;
+
+import com.akmal.springfoodieappbackend.model.OptionSet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * The class is a Spring Data JPA repository that represents the OptionSet entity.
+ * @author Akmal Alikhujaev
+ * @version 1.0
+ * @created 22/10/2021 - 8:30 PM
+ * @project Spring Foodie App Backend
+ * @since 1.0
+ */
+public interface OptionSetRepository extends JpaRepository<OptionSet, Long> {
+}
