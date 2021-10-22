eff0fe04f8a9e5d93195bfb12c34d5ea2ce0d25c
Akmal Alikhujaev <akmal@viaware.nl>
2021-10-22 20:40:02 +0200

    [ADD] Create CustomerDetailsRepository

---
+++ src/main/java/com/akmal/springfoodieappbackend/repository/CustomerDetailsRepository.java
@@ -0,0 +1,15 @@
+package com.akmal.springfoodieappbackend.repository;
+
+import com.akmal.springfoodieappbackend.model.CustomerDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * The class is a Spring Data Repository that represents the CustomerDetails Entity.
+ * @author Akmal Alikhujaev
+ * @version 1.0
+ * @created 22/10/2021 - 8:32 PM
+ * @project Spring Foodie App Backend
+ * @since 1.0
+ */
+public interface CustomerDetailsRepository extends JpaRepository<CustomerDetails, Long> {
+}
