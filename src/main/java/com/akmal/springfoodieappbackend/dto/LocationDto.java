b1eb3d9b5da75bf8834f14454785d59e6aaa99c1
Akmal Alikhujaev <akmal@viaware.nl>
2021-10-22 21:03:18 +0200

    [ADD] Create LocationDto

---
+++ src/main/java/com/akmal/springfoodieappbackend/dto/LocationDto.java
@@ -0,0 +1,13 @@
+package com.akmal.springfoodieappbackend.dto;
+
+/**
+ * DTO class implemented using Java Record Classes representing Location entity
+ * @author Akmal Alikhujaev
+ * @version 1.0
+ * @created 22/10/2021 - 8:47 PM
+ * @project Spring Foodie App Backend
+ * @since 1.0
+ */
+public record LocationDto(double lat,
+                          double lon) {
+}
