ac9271f5c7e485e9a1fa6bf083e0a2c78ea84add
Akmal Alikhujaev <akmal@viaware.nl>
2021-10-22 21:05:51 +0200

    [ADD] Create OptionDto

---
+++ src/main/java/com/akmal/springfoodieappbackend/dto/OptionDto.java
@@ -0,0 +1,16 @@
+package com.akmal.springfoodieappbackend.dto;
+
+import java.math.BigDecimal;
+
+/**
+ * DTO class implemented using Java Record Classes representing Option entity
+ * @author Akmal Alikhujaev
+ * @version 1.0
+ * @created 22/10/2021 - 9:00 PM
+ * @project Spring Foodie App Backend
+ * @since 1.0
+ */
+public record OptionDto(long id,
+                        String name,
+                        BigDecimal price) {
+}
