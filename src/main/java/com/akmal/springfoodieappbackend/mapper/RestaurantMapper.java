c5a56e082dd59fb9c15a91c0bfbf179f1b88d015
Akmal Alikhujaev <akmal@viaware.nl>
2021-10-23 18:31:01 +0200

    [ADD] Create RestaurantMapper
    Created RestaurantMapper to map object from DTO and back

---
+++ src/main/java/com/akmal/springfoodieappbackend/mapper/RestaurantMapper.java
@@ -0,0 +1,109 @@
+package com.akmal.springfoodieappbackend.mapper;
+
+import com.akmal.springfoodieappbackend.dto.*;
+import com.akmal.springfoodieappbackend.model.*;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.NullValueCheckStrategy;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/**
+ * Class represents a contract based on which Mapstruct library generates the
+ * mapper class at runtime. Converts Restaurant Entity to DTO and vice versa
+ *
+ * @author Akmal Alikhujaev
+ * @version 1.0
+ * @created 23/10/2021 - 12:01 PM
+ * @project Spring Foodie App Backend
+ * @since 1.0
+ */
+@Mapper(componentModel = "spring", nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS)
+public abstract class RestaurantMapper {
+  @Autowired
+  private AddressMapper addressMapper;
+  @Autowired
+  private MenuMapper menuMapper;
+  @Autowired
+  private CategoryMapper categoryMapper;
+  @Autowired
+  private OpeningTimeMapper openingTimeMapper;
+
+  @Mapping(target = "address", expression = "java(mapAddressToDto(restaurant.getAddress()))")
+  @Mapping(target = "priceRange", source = "restaurant.priceRange.type")
+  @Mapping(target = "openingTimes", expression = "java(mapOpeningTimesToDto(restaurant.getOpeningTimes()))")
+  @Mapping(target = "categories", expression = "java(mapCategoriesToDto(restaurant.getCategories()))")
+  @Mapping(target = "menus", expression = "java(mapMenusToDto(restaurant.getMenus()))")
+  public abstract RestaurantDto toDto(Restaurant restaurant);
+
+  @Mapping(target = "address", expression = "java(mapToAddress(restaurantDto.address()))")
+  @Mapping(target = "priceRange", expression = "java(mapPriceRange(restaurantDto.name()))")
+  @Mapping(target = "openingTimes", expression = "java(mapToOpeningTimes(restaurantDto.openingTimes()))")
+  @Mapping(target = "categories", expression = "java(mapToCategories(restaurantDto.categories()))")
+  @Mapping(target = "menus", expression = "java(mapToMenus(restaurantDto.menus()))")
+  public abstract Restaurant from(RestaurantDto restaurantDto);
+
+  protected List<OpeningTimeDto> mapOpeningTimesToDto(List<OpeningTime> openingTimes) {
+    return Optional.ofNullable(openingTimes)
+            .orElse(List.of())
+            .stream()
+            .map(this.openingTimeMapper::toDto)
+            .collect(Collectors.toList());
+  }
+
+  protected List<OpeningTime> mapToOpeningTimes(List<OpeningTimeDto> openingTimes) {
+    return Optional.ofNullable(openingTimes)
+            .orElse(List.of())
+            .stream()
+            .map(this.openingTimeMapper::from)
+            .collect(Collectors.toList());
+  }
+
+  protected List<CategoryDto> mapCategoriesToDto(List<Category> categories) {
+    return Optional.ofNullable(categories)
+            .orElse(List.of())
+            .stream()
+            .map(this.categoryMapper::toDto)
+            .collect(Collectors.toList());
+  }
+
+  protected List<Category> mapToCategories(List<CategoryDto> categories) {
+    return Optional.ofNullable(categories)
+            .orElse(List.of())
+            .stream()
+            .map(this.categoryMapper::from)
+            .collect(Collectors.toList());
+  }
+
+  protected List<MenuDto> mapMenusToDto(List<Menu> menus) {
+    return Optional.ofNullable(menus)
+            .orElse(List.of())
+            .stream()
+            .map(this.menuMapper::toDto)
+            .collect(Collectors.toList());
+  }
+
+  protected List<Menu> mapToMenus(List<MenuDto> menus) {
+    return Optional.ofNullable(menus)
+            .orElse(List.of())
+            .stream()
+            .map(this.menuMapper::from)
+            .collect(Collectors.toList());
+  }
+
+
+  protected AddressDto mapAddressToDto(Address address) {
+    return this.addressMapper.toDto(address);
+  }
+
+  protected Address mapToAddress(AddressDto address) {
+    return this.addressMapper.from(address);
+  }
+
+  protected Restaurant.PriceRange mapPriceRange(String type) {
+    return Restaurant.PriceRange.from(type);
+  }
+}
