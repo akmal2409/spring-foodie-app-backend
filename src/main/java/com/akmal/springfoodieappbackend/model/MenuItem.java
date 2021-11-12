83e01036ccc72b4ee71e8ed2a7a2fd3edd5d01cd
Akmal Alikhujaev <akmal@viaware.nl>
2021-11-12 17:53:02 +0100

    [CHANGE] Add helper method to keep both sides of ORM relationship synchronized.
    Added method to add opening times and update their reference to the parent MenuItem.java

--- src/main/java/com/akmal/springfoodieappbackend/model/MenuItem.java
+++ src/main/java/com/akmal/springfoodieappbackend/model/MenuItem.java
@@ -1,14 +1,12 @@
 package com.akmal.springfoodieappbackend.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotBlank;
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -26,16 +24,31 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor(force = true)
 @Builder
+@With
 public class MenuItem {
   @NotBlank(message = "Name is required")
   private final String name;
   @DecimalMin(value = "0.0", message = "Base price must be greater than 0")
   private final BigDecimal basePrice;
   @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "menuItem")
-  private final List<OptionSet> optionSets;
+  private final List<OptionSet> optionSets = new ArrayList<>();
   @ManyToOne(fetch = FetchType.LAZY)
   private final Menu menu;
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private long id;
+
+  /**
+   * <strong>addOptionSet(OptionSet optionSet)</strong> is a helper method that enables the client
+   * to synchronize the both sides of the @OneToMany relationship.
+   * The owning side of the relationship is the {@link OptionSet} class
+   * and therefore, it should manage the persistence of a menu item by itself.
+   * @param optionSet - object representing a set of {@link Option}'s instances
+   * @return immutable copy of the {@link OptionSet} instance with the menu item reference.
+   */
+  public OptionSet addOptionSet(OptionSet optionSet) {
+    final var optionSetWithMenuItem = optionSet.withMenuItem(this);
+    this.optionSets.add(optionSetWithMenuItem);
+    return optionSetWithMenuItem;
+  }
 }
