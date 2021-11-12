f5ce49b5058195432e342761be43b1ac3d0e787f
Akmal Alikhujaev <akmal@viaware.nl>
2021-11-12 17:55:53 +0100

    [CHANGE] Add helper method to keep both sides of ORM relationship synchronized.
    Added a method to add order line items and update their reference to the parent order

--- src/main/java/com/akmal/springfoodieappbackend/model/Order.java
+++ src/main/java/com/akmal/springfoodieappbackend/model/Order.java
@@ -10,9 +10,13 @@ import org.hibernate.annotations.UpdateTimestamp;
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * The {@code Order} class represents a single user's order consisting
+ * of the {@link OrderLineItem} class's instances. Encapsulates all the data
+ * and logic needed to complete the order from a restaurant.
  * @author Akmal Alikhujaev
  * @version 1.0
  * @created 15/10/2021 - 9:34 PM
@@ -36,7 +40,7 @@ public class Order {
   @Embedded
   private final Address address;
   @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "order")
-  private final List<OrderLineItem> orderLineItems;
+  private final List<OrderLineItem> orderLineItems = new ArrayList<>();
   private final BigDecimal totalPrice;
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
@@ -44,5 +48,19 @@ public class Order {
 
   public enum OrderStatus {
     AWAITING_PAYMENT, PROCESSING, PREPARING, DELIVERING, DELIVERED, CANCELED, REJECTED
+  }
+
+  /**
+   * <strong>addOrderLineItems(OrderLineItem orderLineItem)</strong> is a helper method that enables the client
+   * to synchronize the both sides of the @OneToMany relationship.
+   * The owning side of the relationship is the {@link OrderLineItem} class
+   * and therefore, it should manage the persistence of an order by itself.
+   * @param orderLineItem - object representing a single item in the order
+   * @return immutable copy of the {@link OrderLineItem} instance with the order reference.
+   */
+  public OrderLineItem addOrderLineItem(OrderLineItem orderLineItem) {
+    final var itemWithOrder = orderLineItem.withOrder(this);
+    this.orderLineItems.add(itemWithOrder);
+    return itemWithOrder;
   }
 }
