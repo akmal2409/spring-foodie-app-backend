77bb91f46725ac2b0e8a258f569b3b0d6d78af56
Akmal Alikhujaev <akmal@viaware.nl>
2021-11-12 17:52:36 +0100

    [CHANGE] Add helper method to keep both sides of ORM relationship synchronized.
    Added the method to add menu items and update their reference with the current Menu.java

--- src/main/java/com/akmal/springfoodieappbackend/model/Menu.java
+++ src/main/java/com/akmal/springfoodieappbackend/model/Menu.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -28,10 +29,24 @@ public class Menu {
   @JoinColumn(name = "category_id", referencedColumnName = "id")
   private final Category category;
   @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "menu")
-  private final List<MenuItem> menuItems;
+  private final List<MenuItem> menuItems = new ArrayList<>();
   @ManyToOne(fetch = FetchType.LAZY)
   private final Restaurant restaurant;
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private long id;
+
+  /**
+   * <strong>addMenuItem(MenuItem menuItem)</strong> is a helper method that enables the client
+   * to synchronize the both sides of the @OneToMany relationship.
+   * The owning side of the relationship is the {@link MenuItem} class
+   * and therefore, it should manage the persistence of a menu by itself.
+   * @param menuItem - object representing a single menu item in the menu
+   * @return immutable copy of the {@link MenuItem} instance with the menu reference.
+   */
+  public MenuItem addMenuItem(MenuItem menuItem) {
+    final var menuItemWithMenu = menuItem.withMenu(this);
+    this.menuItems.add(menuItemWithMenu);
+    return menuItemWithMenu;
+  }
 }
