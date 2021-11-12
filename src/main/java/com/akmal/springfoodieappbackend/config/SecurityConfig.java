5a5b7650476b538f2e7c6fe1186920e6e21e38f1
Akmal Alikhujaev <akmal@viaware.nl>
2021-11-12 20:34:38 +0100

    [CHANGE] Secured /api/restaurant route
    Allowed only get requests for not authenticated users

--- src/main/java/com/akmal/springfoodieappbackend/config/SecurityConfig.java
+++ src/main/java/com/akmal/springfoodieappbackend/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.akmal.springfoodieappbackend.config;
 
+import com.akmal.springfoodieappbackend.controller.RestaurantController;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -11,7 +13,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests(authorizeRequests -> authorizeRequests.anyRequest().authenticated())
+    http.authorizeRequests(authorizeRequests ->
+                    authorizeRequests
+                            .mvcMatchers(HttpMethod.GET, RestaurantController.BASE_URL, RestaurantController.BASE_URL + "/{id}")
+                            .permitAll()
+                            .anyRequest().authenticated())
             .oauth2ResourceServer().jwt();
   }
 }
