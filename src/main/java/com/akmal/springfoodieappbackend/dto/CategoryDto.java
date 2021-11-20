0865ba2569e553e4258031e89389d014031ae549
Akmal Alikhujaev <akmal@viaware.nl>
2021-11-20 18:10:15 +0100

    [ADD] Implement CategoryController and CategoryService
    Implemented CRUD functionality for CategoryService and Controller.

--- src/main/java/com/akmal/springfoodieappbackend/dto/CategoryDto.java
+++ src/main/java/com/akmal/springfoodieappbackend/dto/CategoryDto.java
@@ -11,5 +11,6 @@ package com.akmal.springfoodieappbackend.dto;
  */
 public record CategoryDto(long id,
                           String name,
-                          String description) {
+                          String description,
+                          int groupId) {
 }
