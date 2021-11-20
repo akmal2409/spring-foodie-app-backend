f2753ab0f569146bdd395c9a879628adf7ed5b51
Akmal Alikhujaev <akmal@viaware.nl>
2021-11-20 17:58:39 +0100

    [ADD] Create a new security config for unit and integration tests.

---
+++ src/main/java/com/akmal/springfoodieappbackend/config/TestSecurityConfig.java
@@ -0,0 +1,27 @@
+package com.akmal.springfoodieappbackend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * The class represents the configuration for unit tests to run.
+ * @author Akmal Alikhujaev
+ * @version 1.0
+ * @created 15/11/2021 - 5:09 PM
+ * @project Spring Foodie App Backend
+ * @since 1.0
+ */
+@Configuration
+@Profile("test")
+public class TestSecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests(authorizeRequests ->
+                    authorizeRequests
+                            .anyRequest()
+                            .permitAll());
+  }
+}
