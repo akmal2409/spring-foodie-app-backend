b42425e91143a92c5f03b3eafb18c9ba15ed798e
Akmal Alikhujaev <akmal@viaware.nl>
2021-11-20 21:27:31 +0100

    [ADD] Custom FileMetaData record class

---
+++ src/main/java/com/akmal/springfoodieappbackend/model/FileMetaData.java
@@ -0,0 +1,12 @@
+package com.akmal.springfoodieappbackend.model;
+
+/**
+ * @author Akmal Alikhujaev
+ * @version 1.0
+ * @created 20/11/2021 - 8:37 PM
+ * @project Spring Foodie App Backend
+ * @since 1.0
+ */
+public record FileMetaData(String location,
+                           String key) {
+}
