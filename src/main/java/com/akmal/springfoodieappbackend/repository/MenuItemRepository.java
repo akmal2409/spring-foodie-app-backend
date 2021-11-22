9148a906c3b0639c833349c575f6884f000fa8d1
Akmal Alikhujaev <akmal@viaware.nl>
2021-11-22 20:56:10 +0100

    [ADD] Custom query in MenuItemRepository.java to find all with matching image ids.

--- src/main/java/com/akmal/springfoodieappbackend/repository/MenuItemRepository.java
+++ src/main/java/com/akmal/springfoodieappbackend/repository/MenuItemRepository.java
@@ -2,6 +2,7 @@ package com.akmal.springfoodieappbackend.repository;
 
 import com.akmal.springfoodieappbackend.model.MenuItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * The class is a Spring Data JPA repository for the MenuItem entity
@@ -13,4 +14,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @since 1.0
  */
 public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {
+
+  /**
+   * Method is using custom JPQL Hibernate query, finds all {@link MenuItem} entities that have
+   * thumbnail image or full image id's matching with the provided one.
+   *
+   * @param imageId of the image.
+   * @return collection of {@link MenuItem} entities.
+   */
+  @Query("SELECT menuItem FROM MenuItem menuItem WHERE menuItem.thumbnailImage.id = ?1 OR menuItem.fullImage.id = ?1")
+  Iterable<MenuItem> findAllByThumbnailImageOrFullImageId(String imageId);
 }
