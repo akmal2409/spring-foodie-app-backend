547011c6b474d6d62938bb981e303dfb81bb0c45
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2022-01-24 18:50:53 +0100

    [ADD] mock-data.sql with mock categories

---
+++ scripts/mock-data.sql
@@ -0,0 +1,73 @@
+INSERT INTO `foodie`.`image`
+VALUES ('1', 'Deals icon', 'https://d4p17acsd5wyj.cloudfront.net/shortcuts/deals.png');
+INSERT INTO `foodie`.`category`
+VALUES (1, 'Deals', 1, 'Deals', '1');
+
+INSERT INTO `foodie`.`image`
+VALUES ('2', 'Top Eats icon', 'https://d4p17acsd5wyj.cloudfront.net/shortcuts/top_eats.png');
+INSERT INTO `foodie`.`category`
+VALUES (2, 'Top Eats', 1, 'Top Eats', '2');
+
+INSERT INTO `foodie`.`image`
+VALUES ('3', 'Sushi icon', 'https://d4p17acsd5wyj.cloudfront.net/shortcuts/cuisines/sushi.png');
+INSERT INTO `foodie`.`category`
+VALUES (3, 'Sushi', 1, 'Sushi', '3');
+
+INSERT INTO `foodie`.`image`
+VALUES ('4', 'Asian icon', 'https://d4p17acsd5wyj.cloudfront.net/shortcuts/cuisines/asian.png');
+INSERT INTO `foodie`.`category`
+VALUES (4, 'Asian', 1, 'Asian', '4');
+
+
+INSERT INTO `foodie`.`image`
+VALUES ('5', 'American icon',
+        'https://d4p17acsd5wyj.cloudfront.net/shortcuts/cuisines/american.png');
+INSERT INTO `foodie`.`category`
+VALUES (5, 'American', 1, 'American', '5');
+
+INSERT INTO `foodie`.`image`
+VALUES ('6', 'Fast Food icon',
+        'https://d4p17acsd5wyj.cloudfront.net/shortcuts/cuisines/fastfood.png');
+INSERT INTO `foodie`.`category`
+VALUES (6, 'Fast Food', 1, 'Fast Food', '6');
+
+INSERT INTO `foodie`.`image`
+VALUES ('7', 'Japanese icon',
+        'https://d4p17acsd5wyj.cloudfront.net/shortcuts/cuisines/japanese.png');
+INSERT INTO `foodie`.`category`
+VALUES (7, 'Japanese', 1, 'Japanese', '7');
+
+INSERT INTO `foodie`.`image`
+VALUES ('8', 'Italian icon', 'https://d4p17acsd5wyj.cloudfront.net/shortcuts/cuisines/italian.png');
+INSERT INTO `foodie`.`category`
+VALUES (8, 'Italian', 1, 'Italian', '8');
+
+INSERT INTO `foodie`.`image`
+VALUES ('9', 'Pizza icon', 'https://d4p17acsd5wyj.cloudfront.net/shortcuts/cuisines/pizza.png');
+INSERT INTO `foodie`.`category`
+VALUES (9, 'Pizza', 1, 'Pizza', '9');
+
+INSERT INTO `foodie`.`image`
+VALUES ('10', 'Chinese icon',
+        'https://d4p17acsd5wyj.cloudfront.net/shortcuts/cuisines/chinese.png');
+INSERT INTO `foodie`.`category`
+VALUES (10, 'Chinese', 1, 'Chinese', '10');
+
+INSERT INTO `foodie`.`image`
+VALUES ('11', 'Bubble Tea icon',
+        'https://d4p17acsd5wyj.cloudfront.net/shortcuts/cuisines/bubbletea.png');
+INSERT INTO `foodie`.`category`
+VALUES (11, 'Bubble Tea', 1, 'Bubble Tea', '11');
+
+INSERT INTO `foodie`.`image`
+VALUES ('12', 'Comfort food icon',
+        'https://d4p17acsd5wyj.cloudfront.net/shortcuts/cuisines/comfortfood.png');
+INSERT INTO `foodie`.`category`
+VALUES (12, 'Comfort food', 1, 'Comfort food', '12');
+
+INSERT INTO `foodie`.`image`
+VALUES ('13', 'Sandwich icon',
+        'https://d4p17acsd5wyj.cloudfront.net/shortcuts/cuisines/sandwich.png');
+INSERT INTO `foodie`.`category`
+VALUES (13, 'Sandwich', 1, 'Sandwich', '13');
+
