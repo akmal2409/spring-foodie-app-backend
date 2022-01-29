ffff3ddbc8b8b35c49faddbd6b5273d3fa12b1d0
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2022-01-29 19:51:32 +0100

    [CHANGE] OpeningTime.java cache DateTimeFormatter for common time conversions. Provide method for time to string conversion.
    Cached the instance of the DateTimeFormatter with a pattern "HH:mm" by declaring it as static because it will be accessed for every instance of a restaurant that is being fetched.

--- src/main/java/com/akmal/springfoodieappbackend/model/OpeningTime.java
+++ src/main/java/com/akmal/springfoodieappbackend/model/OpeningTime.java
@@ -1,12 +1,24 @@
 package com.akmal.springfoodieappbackend.model;
 
-import lombok.*;
-
-import javax.persistence.*;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.With;
 
 /**
- * Class represents opening times of a facility. Day is an integer from 1 to 7 where 1 is Sunday.
+ * Class represents opening times of a facility. Day is an integer from 1 to 7 where 1 is Monday.
  *
  * @author Akmal Alikhujaev
  * @version 1.0
@@ -21,6 +33,8 @@ import java.time.LocalTime;
 @Builder
 @With
 public class OpeningTime {
+  public static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
+
   @Column(name = "opening_day")
   private final int day;
 
@@ -35,4 +49,16 @@ public class OpeningTime {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private long id;
+
+  /**
+   * The method returns the <em>openFrom</em> and <em>openTill</em> separated by a hyphen. E.g.
+   * <strong>11:20 - 22:30</strong>
+   *
+   * @return
+   */
+  public String toRangeString() {
+    return String.format(
+        "%s - %s",
+        this.openFrom.format(formatter).toString(), this.openTill.format(formatter).toString());
+  }
 }
