a1de9efd7ccddb51fc7e40e2b97c04f5e13258e4
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2022-01-30 17:48:40 +0100

    [CHANGE] /restaurants to serve data from elasticsearch datastore with additional query parameters.

--- src/main/java/com/akmal/springfoodieappbackend/controller/RestaurantController.java
+++ src/main/java/com/akmal/springfoodieappbackend/controller/RestaurantController.java
@@ -1,16 +1,26 @@
 package com.akmal.springfoodieappbackend.controller;
 
+import static com.akmal.springfoodieappbackend.shared.http.ResponseEntityConverter.ok;
+
 import com.akmal.springfoodieappbackend.dto.RestaurantDto;
+import com.akmal.springfoodieappbackend.service.RestaurantSearchService;
 import com.akmal.springfoodieappbackend.service.RestaurantService;
+import java.util.List;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-
-import static com.akmal.springfoodieappbackend.shared.http.ResponseEntityConverter.ok;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * The RestController class containing methods of different HTTP request type handlers. The data
@@ -28,15 +38,17 @@ import static com.akmal.springfoodieappbackend.shared.http.ResponseEntityConvert
 public class RestaurantController {
   public static final String BASE_URL = "/api/restaurants";
   private final RestaurantService restaurantService;
+  private final RestaurantSearchService restaurantSearchService;
 
   @GetMapping
   public Page<RestaurantDto> findAll(
-      @RequestParam(defaultValue = "0") int page,
-      @RequestParam(defaultValue = "10") int size,
+      @RequestParam(defaultValue = "0", required = false) int page,
+      @RequestParam(defaultValue = "10", required = false) int size,
       @RequestParam String city,
-      @RequestParam String country) {
-
-    return this.restaurantService.findAllByCountry(page, size, country, city);
+      @RequestParam String country,
+      @RequestParam(value = "sort", required = false, defaultValue = "") List<String> sort) {
+    return this.restaurantSearchService.findAllByCountryAndCityRankByOpeningTimeWithinNow(
+        country, city, page, size, sort);
   }
 
   @GetMapping("/{id}")
